62d8e0f617f8bb2e1e092ca3663c9b14488cceb1
Lamia1406 <101535283+Lamia1406@users.noreply.github.com>
2022-11-12 14:46:54 +0100

    Add files via upload

---
+++ homeTeacher.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import "../styles/home.css";
+import Header from "../partials/header";
+import SideMenu from "../partials/side-menu";
+import { useNavigate } from "react-router-dom";
+import { useEffect} from "react";
+function homeTeacher(){
+    const navigate = useNavigate();
+  const accountType = window.localStorage.getItem("accountType");
+
+  useEffect(() => {
+    const loggedIn = window.localStorage.getItem('loggedIn');
+    if (!loggedIn) {
+      navigate('/login');
+    }
+  }, []);
+    <div className="home-page" id='homeTeacher'>
+    <Header />
+    <div className="home">
+      <SideMenu type={accountType} page='homeTeacher'/>
+      <div className="main-page">
+      <div className="main-page">
+        <div className="welcome-belala-nabil">
+          <div className="welcome">
+            <p>Welcome</p>
+            <p>Belala Nabil</p>
+          </div>
+          <div className="start-new-session">
+            <button> 
+              <img src={plus}></img>
+            <p>Start new session</p>
+            </button>
+          </div>
+        </div>
+        <div className="profile-div">
+            <img src={avatar} alt="avatar" className="big-icon " />
+
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+}
+
+export default homeTeacher;
